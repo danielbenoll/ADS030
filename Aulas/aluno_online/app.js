ca0e1a457e63264e4ed0e76a27872f1ba3549e49
daniel <danielbenoll@hotmail.com>
2020-10-16 18:05:19 -0300

    Atividade 3 hotfix/issue3

--- Aulas/aluno_online/app.js
+++ Aulas/aluno_online/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var requerimentosRouter = require('./routes/requerimentos');
 var notasRouter = require('./routes/notas');
 var faltasRouter = require('./routes/faltas');
+var boletosRouter = require('./routes/boletos');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/requerimentos', requerimentosRouter);
 app.use('/notas', notasRouter);
 app.use('/faltas', faltasRouter);
+app.use('/boletos', boletosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
